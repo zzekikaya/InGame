ead2b77bc9b9c4df83119495fe4ad78fd2dcd06a
Zeki KAYA <zzeki.kaya@gmail.com>
2019-03-18 03:28:37 +0300

    productService_test added

---
+++ InGame.Tests/InGame.Core.Test/ProductTest.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using InGame.Core.Entities;
+using InGame.Core.Interfaces;
+using InGame.Core.Services;
+using Moq;
+using Xunit;
+
+namespace InGame.Tests.CoreTest
+{
+    public class ProductTest
+    {
+        private Mock<IAsyncRepository<Product>> _mockProductRepo;
+
+        public ProductTest()
+        {
+            _mockProductRepo = new Mock<IAsyncRepository<Product>>();
+        }
+
+        [Fact]
+        public async Task ProductGetById_Test()
+        {
+            var product = new Product();
+            _mockProductRepo.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(product);
+            var productService = new ProductService(_mockProductRepo.Object, null, null);
+
+            await productService.GetByIdAsync(product.Id);
+
+            _mockProductRepo.Verify(x => x.GetByIdAsync(product.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task Insert_Product_Test()
+        {
+            var product = new Product()
+            {
+                Name = "silah",
+                Description = "ücretsiz",
+                Price = 5000,
+                SubCategoryID = 1
+            };
+            var productService = new ProductService(_mockProductRepo.Object, null, null);
+
+            await productService.AddAsync(product);
+
+            _mockProductRepo.Verify(x => x.AddAsync(product));
+        }
+
+        [Fact]
+        public async Task Update_Product_Test()
+        {
+            var product = new Product();
+            _mockProductRepo.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(product);
+            var productService = new ProductService(_mockProductRepo.Object, null, null);
+
+            await productService.UpdateAsync(product);
+
+            _mockProductRepo.Verify(x => x.UpdateAsync(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Delete_Product_Test()
+        {
+            var product = new Product();
+            _mockProductRepo.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(product);
+            var productService = new ProductService(_mockProductRepo.Object, null, null);
+
+            await productService.DeleteAsync(product);
+
+            _mockProductRepo.Verify(x => x.DeleteAsync(It.IsAny<Product>()), Times.Once);
+        }
+    }
+}
---
+++ InGame.Tests/LoginTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using InGame.Core.Entities;
+using InGame.Infrastructure;
+using InGame.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace InGame.Tests
+{
+    public class LoginTest
+    {
+        [Fact]
+        public async Task LogsInSampleUser()
+        {
+            var services = new ServiceCollection(); 
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer("DefaultConnection");
+            });
+            var serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            // Create a scope to obtain a reference to the database
+            // context (AppIdentityDbContext).
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+
+                try
+                {
+                    // seed sample user data
+                    var userManager = scopedServices.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    AppIdentityDbContextSeed.SeedAsync(userManager).Wait();
+
+                    var signInManager = scopedServices.GetRequiredService<SignInManager<ApplicationUser>>();
+
+                    var email = "zzeki@gmail.com";
+                    var password = "Pass@word1";
+
+                    var result = await signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+                    Assert.True(result.Succeeded);
+
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+}
